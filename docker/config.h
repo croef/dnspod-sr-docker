bd723d84175604a2134827357d897000019581f6
croef <changrong185@gmail.com>
2018-08-20 10:14:58 +0800

    init dnspod-sr

---
+++ docker/config.h
@@ -0,0 +1,8 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define SR_CONFIG_FILE "/root/dnspod/sr.conf"
+#define SR_ROOT_FILE "/root/dnspod/root.z"
+#define SR_RECORDS_FILE "/root/dnspod/records.z"
+
+#endif
